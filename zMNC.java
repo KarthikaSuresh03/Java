a5ec1f66df89378f6b9d65fb9b40f29244aebe27
Karthika Suresh <147239264+KarthikaSuresh03@users.noreply.github.com>
2023-12-19 22:04:08 +0530

    Update zMNC.java

--- zMNC.java
+++ zMNC.java
@@ -2,17 +2,63 @@
 
 import java.sql.*;
 import java.util.*;
+import javax.swing.*;
+import java.awt.event.*;
 
+class empGUI extends JFrame implements ActionListener {
+
+    JButton get;
+    JTextField ip, op;
+    JLabel idL, detailsL;
+    Class.forName("com.mysql.cj.jdbc.Driver");
+    Connection con = DriverManager.getConnection("jdbc.mysql://localhost:3306/mnc","root","root");
+    Statement stm = con.createStatement();
+
+    empGUI() {
+
+        super("Employee Details");
+        setLayout(null);
+        setSize(500, 400);
+        get = new JButton("Get Details");
+        ip = new JTextField();
+        op = new JTextField();
+        idL = new JLabel("Enter Employee ID");
+        detailsL = new JLabel("Details");
+
+        add(get);
+        add(ip);
+        add(op);
+        add(idL);
+        add(detailsL);
+
+        idL.setBounds(10, 40, 200, 20);
+        ip.setBounds(10, 70, 200, 20);
+        get.setBounds(10, 100, 200, 25);
+        detailsL.setBounds(10, 135, 200, 20);
+        op.setBounds(10, 165, 500, 20);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        get.addActionListener(this);
+    }
+
+    public void actionPerformed(ActionEvent ae) {
+
+        int id = Integer.parseInt(ip.getText());
+
+        String qry = "select * from employee where empid = " + id;
+        ResultSet rs = stm.executeQuery(qry);
+        op.setText(rs.getInt(1) + "\t" + rs.getString(2) + "\t" + rs.getString(3) + "\t" + rs.getString(4) + "\t" + rs.getInt(5) + "\t" + rs.getDouble(6));
+    }
+}
 public class zMNC {
     
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection con = DriverManager.getConnection("jdbc.mysql://localhost:3306/college","root","pass");
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc.mysql://localhost:3306/mnc","root","root");
         Statement stm = con.createStatement();
         
         Scanner scan = new Scanner(System.in);
-        System.out.println("Microsoft Corporation")
+        System.out.println("Microsoft Corporation");
         System.out.println("---------------------");
         System.out.println("\t1 Insert Employee Details");
         System.out.println("\t2 Display Employee Details");
@@ -57,12 +103,13 @@ public class zMNC {
                 System.out.println("\nEnter your Choice");
                 int display = scan.nextInt();
 
+				ResultSet rs;
                 switch(display) {
 
                     case 1 : {
 
                         String qry = "select * from employee order by salary desc";
-                        ResultSet rs = stm.executeQuery(qry);
+                        rs = stm.executeQuery(qry);
                         while (rs.next()) {
 
                             System.out.println(rs.getInt(1) + "\t" + rs.getString(2) + "\t" + rs.getString(3) + "\t" + rs.getString(4) + "\t" + rs.getInt(5) + "\t" + rs.getDouble(6));
@@ -70,11 +117,7 @@ public class zMNC {
                     }     
                     case 2 : {
 
-                        System.out.println("Enter the Employee ID");
-                        int id = scan.nextInt();
-
-                        String qry = "select * from employee where empid = " + id;
-                        ResultSet rs = stm.executeQuery(qry);
+                        empGUI g = new empGUI();
                     }
                     case 3 : {
 
@@ -82,7 +125,7 @@ public class zMNC {
                         String nation = scan.nextLine();
 
                         String qry = "select * from employee where nationality = " + nation;
-                        ResultSet rs = stm.executeQuery(qry);                
+                        rs = stm.executeQuery(qry);                
                     }
                     case 4 : {
 
@@ -90,7 +133,7 @@ public class zMNC {
                         String des = scan.nextLine();
 
                         String qry = "select * from employee where designation = " + des;
-                        ResultSet rs = stm.executeQuery(qry);                
+                        rs = stm.executeQuery(qry);                
                     }
                     default: {
 
